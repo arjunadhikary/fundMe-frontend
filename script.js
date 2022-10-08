b0e79f24d58ff74a75450ff385116c83416f4a0f
arjunadhikary <reasonworld12345@gmail.com>
2022-10-08 16:09:37 +0545

    feat: :zap: v1.0 basic working

--- script.js
+++ script.js
@@ -5,11 +5,40 @@ const connectButton = document.getElementById('connect-wallet');
 const fundMeFormField = document.getElementById('fund');
 const fundMeButton = document.getElementById('fundme-button');
 const withdrawButton = document.getElementById('withdraw-button');
+const refreshBalance = document.getElementById('refresh-balance');
 
+const amountInWallet = async () => {
+  const { contract, provider } = await connect();
+  const balance = await provider.getBalance(contract.address);
+  return balance;
+};
+const getBalanceHelper = async () => {
+  const balance = await amountInWallet();
+
+  document.getElementById('balance-amount').textContent =
+    'you currently have \t' + ethers.utils.formatEther(balance) + ' ether \n';
+};
+
+refreshBalance.addEventListener('click', async () => {
+  await getBalanceHelper();
+});
+
+//get balance on page load
+document.addEventListener('DOMContentLoaded', async () => {
+  const { contract, provider } = await connect();
+  const balance = await provider.getBalance(contract.address);
+  console.log(balance.toString());
+  document.getElementById('balance-amount-loading').remove();
+  document.getElementById('balance-amount').textContent =
+    'you currently have \t' + ethers.utils.formatEther(balance) + ' ether\n';
+});
+
+//withdraw
 withdrawButton.addEventListener('click', async () => {
   await withdraw();
 });
 
+//fund
 fundMeButton.addEventListener('click', async () => {
   const amountToBeFunded = fundMeFormField.value;
   //validate
@@ -18,8 +47,8 @@ fundMeButton.addEventListener('click', async () => {
   await fund(ethers.utils.parseEther(amountToBeFunded));
 });
 
-let fundMeContact;
 connectButton.onclick = connect;
+
 async function connect() {
   if (typeof window.ethereum !== 'undefined') {
     //connect to the wallet
@@ -30,7 +59,10 @@ async function connect() {
       await provider.send('eth_requestAccounts', []);
 
       const signer = await provider.getSigner();
-      fundMeContact = new ethers.Contract(CONTRACT_ADDRESS, ABI, signer);
+      return {
+        contract: new ethers.Contract(CONTRACT_ADDRESS, ABI, signer),
+        provider,
+      };
     } catch (error) {
       console.log(error.message);
     }
@@ -41,21 +73,31 @@ async function connect() {
 
 async function withdraw() {
   try {
-    const transactionResponse = await fundMeContact.optimizedwithdraw();
+    const { contract } = await connect();
+    const transactionResponse = await contract.optimizedwithdraw();
     const recepit = await transactionResponse.wait(1);
     console.log(recepit);
-  } catch (error) {}
+    document.getElementById(
+      'withdraw-text'
+    ).textContent = ` Amount ${await amountInWallet()} have been withdrawn hurray!!!! `;
+    await getBalanceHelper();
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 async function fund(amount) {
   try {
-    const transactionResponse = await fundMeContact.fund({ value: amount });
+    const { contract } = await connect();
+
+    const transactionResponse = await contract.fund({ value: amount });
     const { to, from } = await transactionResponse.wait(1);
     window.alert(
       `Transfered ${ethers.utils.formatEther(
         amount
       )} ethers from ${from} to ${to} sucessfully`
     );
+    await getBalanceHelper();
   } catch (error) {
     console.log(error);
   }
